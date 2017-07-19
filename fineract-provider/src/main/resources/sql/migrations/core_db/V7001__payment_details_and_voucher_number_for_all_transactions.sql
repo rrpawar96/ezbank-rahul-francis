0a8485e5862e4419eb3384e9e430a71f31dcf76c
Usman Khaliq <usman.khaliq91@gmail.com>
2017-07-19 11:17:10 +0000

    added script that adds the two fields in the m_payment_details table

--- fineract-provider/src/main/resources/sql/migrations/core_db/V7001__payment_details_and_voucher_number_for_all_transactions.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V7001__payment_details_and_voucher_number_for_all_transactions.sql
@@ -16,7 +16,5 @@
 -- specific language governing permissions and limitations
 -- under the License.
 --
-
-ALTER TABLE `m_payment_details`
-ADD COLUMN `voucher_number` BIGINT(20) NULL DEFAULT NULL,
-ADD COLUMN 'payment_description'  VARCHAR(1000) NULL DEFAULT NULL;
+ALTER TABLE `m_payment_detail` ADD COLUMN `voucher_number` BIGINT(20) NULL DEFAULT NULL;
+ALTER TABLE `m_payment_detail` ADD COLUMN `payment_description` VARCHAR(1000) NULL DEFAULT NULL;
