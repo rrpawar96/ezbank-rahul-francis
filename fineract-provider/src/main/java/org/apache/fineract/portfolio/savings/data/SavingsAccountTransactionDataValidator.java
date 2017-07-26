d4a6636a33afe0b7d4165a37385e0e24da57d0e3
Usman Khaliq <usman.khaliq91@gmail.com>
2017-07-26 20:56:04 +0000

    added voucherNumberParamName and paymentDescriptionParamName to validatePaymentTypeDetail

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/data/SavingsAccountTransactionDataValidator.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/data/SavingsAccountTransactionDataValidator.java
@@ -30,7 +30,11 @@ import static org.apache.fineract.portfolio.savings.SavingsApiConstants.transact
 import static org.apache.fineract.portfolio.savings.SavingsApiConstants.transactionDateParamName;
 import static org.apache.fineract.portfolio.savings.SavingsApiConstants.withdrawBalanceParamName;
 import static org.apache.fineract.portfolio.savings.SavingsApiConstants.SAVINGS_ACCOUNT_HOLD_AMOUNT_REQUEST_DATA_PARAMETERS;
-import static org.apache.fineract.portfolio.savings.SavingsApiConstants.SAVINGS_ACCOUNT_RESOURCE_NAME;
+import static org.apache.fineract.portfolio.savings.SavingsApiConstants.SAVINGS_ACCOUNT_RESOURCE_NAME; 
+import static org.apache.fineract.portfolio.savings.SavingsApiConstants.voucherNumberParamName;
+import static org.apache.fineract.portfolio.savings.SavingsApiConstants.paymentDescriptionParamName;
+
+
 
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
